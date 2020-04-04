b2f22b1dc2a5956bdb2468a872b80e6ddd06c180
Peyton <peyton.sandridge@gmail.com>
2020-04-03 23:29:06 -0400

    Reverted old changes

---
+++ Vertex.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "Rendershape.h"
+#include "GraphicsLib.h"
+struct Vertex {
+	int x;
+	int y;
+	Color color;
+};
---
+++ one_pixel.cpp
@@ -0,0 +1,13 @@
+#include "GraphicsLib.h"
+
+//
+// Example 1 - Hello World
+//
+// Draws a single pixel to the middle of the screen.
+//
+
+
+void main()
+{
+	SetPixel(80, 60, Yellow);
+}
